6395b037b596c46f240328d35ae77ca6284751c8
lionirdeadman <lionirdeadman@lionirdeadman.pc>
2019-02-19 02:02:52 -0500

    Add Rule 34, Add Booru board settings, Change Exception handling and add comments.

--- booru/booru.py
+++ booru/booru.py
@@ -28,7 +28,7 @@ class Booru(BaseCog):
     def __init__(self):
         self.config = Config.get_conf(self, identifier=4894278742742)
         default_global = {"filters": [], "nsfw_filters": []}
-        default_guild = {"filters": [], "nsfw_filters": ["loli", "shota"]}
+        default_guild = {"filters": [], "nsfw_filters": ["loli", "shota"], "boards": ["dan", "gel", "kon", "yan"]}
         self.config.register_global(**default_global)
         self.config.register_guild(**default_guild)
 
@@ -44,10 +44,13 @@ class Booru(BaseCog):
         log.debug(tag)
 
         # Image board fetcher
-        yan_data, gel_data, kon_data, dan_data = await asyncio.gather(self.fetch_yan(ctx, tag), self.fetch_gel(ctx, tag), self.fetch_kon(ctx, tag), self.fetch_dan(ctx, tag))
+        boards = await self.config.guild(ctx.guild).boards()
+        if boards == []:
+            await ctx.send("There no image boards, please use [p]booruset guild boards to set them.")
+            return
 
-        # Fuse multiple image board data
-        data = yan_data + gel_data + kon_data + dan_data
+        all_data = await asyncio.gather(*(getattr(self, f'fetch_{board}')(ctx, tag) for board in boards))
+        data = [item for board_data in all_data for item in board_data]
 
         # Filter data without using up requests space
         data = await self.filter_posts(ctx, data)
@@ -135,6 +138,27 @@ class Booru(BaseCog):
         # Done sending requests, time to show it
         await self.show_booru(ctx, data)
 
+    @commands.command()
+    async def r34(self, ctx, *, tag=None):
+        """Shows a image board entry based on user query from Rule34"""
+
+        tag = await self.filter_tags(ctx, tag)
+
+        if tag is None:
+            return
+
+        log.debug(tag)
+
+        # Image board fetcher
+        data = await self.fetch_r34(ctx, tag)
+        print (data)
+
+        # Filter data without using up requests space
+        data = await self.filter_posts(ctx, data)
+
+        # Done sending requests, time to show it
+        await self.show_booru(ctx, data)
+
     async def filter_tags(self, ctx, tag):
         # Checks if there is a tag and defaults depending on channel
         if tag is not None:
@@ -180,11 +204,14 @@ class Booru(BaseCog):
         guild_nsfw_filters = set(await guild_group.nsfw_filters())
         guild_filters = set(await guild_group.filters())
 
+        # Fuse both global and guild for cleaner use
         filters = global_filters | guild_filters
         nsfw_filters = global_nsfw_filters | guild_nsfw_filters
 
+        # Set variable because
         filtered_data = []
 
+        # Filter the content
         for booru in data:
             booru_tags_string = booru.get("tags") or booru.get("tag_string") or "N/A"
             booru_tags = set(booru_tags_string.split())
@@ -196,6 +223,7 @@ class Booru(BaseCog):
                 continue
 
             filtered_data.append(booru)
+
         return filtered_data
 
     async def fetch_from_booru(self, urlstr, provider):  # Handles provider data and fetcher responses
@@ -203,10 +231,8 @@ class Booru(BaseCog):
         async with aiohttp.ClientSession() as session:
             async with session.get(urlstr) as url:
                 try:
-                    content = await url.json()
-                except ValueError:
-                    content = []
-                except aiohttp.client_exceptions.ContentTypeError:
+                    content = await url.json(content_type=None)
+                except (ValueError, aiohttp.ContentTypeError):
                     content = []
         if not content or content == [] or content is None or (type(content) is dict and "success" in content.keys() and content["success"] == False):
             content = []
@@ -238,6 +264,11 @@ class Booru(BaseCog):
         log.debug(urlstr)
         return await self.fetch_from_booru(urlstr, "Danbooru")
 
+    async def fetch_r34(self, ctx, tags):  # Rule34 fetcher
+        urlstr = "https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=" + "+".join(tags)
+        log.debug(urlstr)
+        return await self.fetch_from_booru(urlstr, "Rule34")
+
     async def show_booru(self, ctx, data):  # Shows various info in embed
         mn = len(data)
         if mn == 0:
@@ -261,7 +292,7 @@ class Booru(BaseCog):
                 booru_score = booru.get("score") or "N/A"
 
                 # Set variables for file url
-                file_url = booru.get("file_url")
+                file_url = booru.get("file_url") or "https://us.rule34.xxx//images/" + booru.get("directory") + "/" + booru.get("image")
                 booru_url = file_url
 
                 # Set variable for post link
@@ -269,12 +300,15 @@ class Booru(BaseCog):
                     booru_post = "https://konachan.com/post/show/" + str(booru.get("id"))
                 if booru["provider"] == "Gelbooru":
                     booru_post = "https://gelbooru.com/index.php?page=post&s=view&id=" + str(booru.get("id"))
+                if booru["provider"] == "Rule34":
+                    booru_post = "https://rule34.xxx/index.php?page=post&s=view&id=" + str(booru.get("id"))
                 if booru["provider"] == "Yandere":
                     booru_post = "https://yande.re/post/show/" + str(booru.get("id"))
                 if booru["provider"] == "Danbooru":
                     booru_post = "https://danbooru.donmai.us/posts/" + str(booru.get("id"))
 
-                color = {"Gelbooru": 3395583, "Danbooru": 3395583, "Konachan": 8745592, "Yandere": 2236962}
+                # Set colour for each board
+                color = {"Gelbooru": 3395583, "Danbooru": 3395583, "Konachan": 8745592, "Yandere": 2236962, "Rule34": 339933}
 
                 embed = discord.Embed()
                 embed.color = color[booru["provider"]]
@@ -466,6 +500,7 @@ class Booru(BaseCog):
         config_filters = set(config_filters)
         config_filters.update(filter)
 
+        # Check variables to see where they come from and do the right thing
         if "all" in origin:
             await self.config.guild(ctx.guild).filters.set(list(config_filters))
         if "nsfw" in origin:
@@ -482,6 +517,7 @@ class Booru(BaseCog):
 
         config_filters = set(config_filters)
 
+        # Check variables to see where they come from and do the right thing
         if "all" in origin:
             await self.config.guild(ctx.guild).filters.set(list(set(config_filters) - set(filter)))
         if "nsfw" in origin:
@@ -491,3 +527,73 @@ class Booru(BaseCog):
         if "nsfw" in origin and "global" in origin:
             await self.config.nsfw_filters.set(list(set(config_filters) - set(filter)))
         await ctx.send("The filters have been removed!")
+
+    @_guild.group(name="boards")
+    @checks.admin_or_permissions(manage_guild=True)
+    async def _guild_boards(self, ctx):
+        """Commands pertaining to which boards are shown in booru"""
+        pass
+
+    @_guild_boards.command(name="show")
+    @checks.admin_or_permissions(manage_guild=True)
+    async def _guild_boards_show(self, ctx):
+        """Show booru boards"""
+        boards = await self.config.guild(ctx.guild).boards()
+        if not boards:
+            await ctx.send("There are currently no boards shown in booru")
+        else:
+            await ctx.send(f"Current boards shown in booru are: ```{','.join(boards)}```")
+
+    @_guild_boards.command(name="add")
+    @checks.is_owner()
+    async def _global_boards_add(self, ctx, *, boards):
+        """Add image boards to booru"""
+
+        # Load config
+        config_boards = await self.config.guild(ctx.guild).boards()
+
+        # Filter input
+        boards = await self.boards_filter(boards)
+        if boards is None:
+            await ctx.send("Reminder that the board names that can be used are dan, gel, kon, yan and r34. Please try again")
+            return
+
+        # Fuse input and config
+        config_boards = set(config_boards)
+        config_boards.update(boards)
+
+        # Set new config
+        await self.config.guild(ctx.guild).boards.set(list(config_boards))
+        await ctx.send("The boards have been added.")
+
+    @_guild_boards.command(name="remove")
+    @checks.is_owner()
+    async def _global_boards_remove(self, ctx, *, boards):
+        """Remove image boards to booru"""
+
+        # Load config
+        config_boards = await self.config.guild(ctx.guild).boards()
+
+        # Filter input
+        boards = await self.boards_filter(boards)
+        if boards is None:
+            await ctx.send("Reminder that the board names that can be used are dan, gel, kon, yan. Please try again")
+            return
+
+        # Set new config
+        config_boards = set(config_boards)
+        await self.config.guild(ctx.guild).boards.set(list(set(config_boards) - set(boards)))
+        await ctx.send("The boards have been removed.")
+
+    async def boards_filter(self, boards):
+
+        boards = set(boards.split(" "))
+
+        # Set variable to see which are good
+        correct_board_names = ["dan", "gel", "kon", "yan", "r34"]
+
+        # Check if good
+        if boards & set(correct_board_names):
+            return boards
+        else:
+            return
