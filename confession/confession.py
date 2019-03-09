0f568928cedc9f59a8e05673d032970179590726
lionirdeadman <lionirdeadman@lionirdeadman.pc>
2019-03-08 22:13:16 -0500

    Make it DM-only and handle that use-case

--- confession/confession.py
+++ confession/confession.py
@@ -1,5 +1,7 @@
 from redbot.core import commands, checks, Config
 from redbot.core.utils.predicates import ReactionPredicate, MessagePredicate
+from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
+import contextlib
 import discord
 
 
@@ -37,22 +39,71 @@ class Confession(BaseCog):
 
     @commands.command()
     async def confess(self, ctx, *, confession):
-        """Confess your dirty sins"""
+        """Confess your dirty sins
 
-        rooms = await self.config.guild(ctx.guild).confession_rooms()
+        It'll ask you which guild to confess in if you have more than one with a confession
+        """
 
-        if rooms == "" or rooms is None:
-            return await ctx.send("The room has not been set.")
+        async def select_guild(ctx: commands.Context, pages: list, controls: dict, message: discord.Message, page: int, timeout: float, emoji: str):
+            # Clean up
+            with contextlib.suppress(discord.NotFound):
+                await message.delete()
+            # Send it off to this function so it sends to initiate search after selecting subdomain
+            await self.selected_guild(ctx, user_guilds, confession, page)
+            return None
 
-        for channel in ctx.guild.text_channels:
+        if bool(ctx.guild):
+            await ctx.send("You should do this in DMs!")
+            try :
+                await ctx.message.delete()
+            except:
+                pass
+            return
+
+        all_guilds = ctx.bot.guilds
+        user_guilds = []
+        for guild in all_guilds:
+            if guild.get_member(ctx.message.author.id):
+                room = await self.config.guild(guild).confession_rooms()
+                print(room)
+                if room is not None:
+                    user_guilds.append(guild)
+
+        if len(user_guilds) == 0:
+            await ctx.author.send("No server with a confession room, ask your server owners to set it up!")
+        if len(user_guilds) == 1:
+            await self.send_confession(ctx, user_guilds[0], confession)
+        else:
+            SELECT_DOMAIN = {"\N{WHITE HEAVY CHECK MARK}": select_guild}
+
+            # Create dict for controls used by menu
+            SELECT_CONTROLS = {}
+            SELECT_CONTROLS.update(DEFAULT_CONTROLS)
+            SELECT_CONTROLS.update(SELECT_DOMAIN)
+
+            embeds = []
+            for guild in user_guilds:
+                embed = discord.Embed()
+                embed.title = "Where do you want to confess?"
+                embed.description = guild.name
+                embeds.append(embed)
+
+            await menu(ctx, pages=embeds, controls=SELECT_CONTROLS, message=None, page=0, timeout=20)
+
+    async def selected_guild(self, ctx, user_guilds, confession, page):
+
+        confession_guild = user_guilds[page]
+        await self.send_confession(ctx, confession_guild, confession)
+
+    async def send_confession(self, ctx, confession_guild, confession):
+
+        rooms = await self.config.guild(confession_guild).confession_rooms()
+
+        for channel in confession_guild.text_channels:
             if rooms == channel.id:
                 confession_room = channel
 
         try:
-            await ctx.message.delete()
-        except:
-            await ctx.send("I couldn't delete your message, sorry!")
-        try:
             await confession_room.send(confession)
         except:
-            await ctx.send("I don't have permission to this room or something went wrong")
+            await ctx.author.send("I don't have permission to this room or something went wrong")
