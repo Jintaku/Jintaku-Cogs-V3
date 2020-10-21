9fafae1c4c9367d79d5502341fa31a94886bf2e3
lionirdeadman <lionirdeadman@lionirdeadman.pc>
2020-10-21 12:27:08 -0400

    Add tv command (it's duplicated but it works)

--- imdb/imdb.py
+++ imdb/imdb.py
@@ -31,7 +31,7 @@ class Imdb(BaseCog):
     @commands.command()
     @commands.bot_has_permissions(embed_links=True, add_reactions=True)
     async def movie(self, ctx, *, title):
-        """Search a movie"""
+        """Search for a movie"""
 
         # Get API key
         apikey = await self.config.apikey()
@@ -43,7 +43,79 @@ class Imdb(BaseCog):
         url = "http://www.omdbapi.com/?"+urlencode({
             "apikey": apikey,
             "s": title,
-            "plot": "short"
+            "plot": "short",
+            "type": "movie"
+        })
+        headers = {"accept": "application/json"}
+
+        # Queries api for a movie
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url=url, headers=headers) as response:
+                data = await response.json()
+
+        # Handle if nothing is found
+        if data['Response'] == "False":
+            await ctx.send("I couldn't find anything! You probably didn't set your API key properly!")
+            return
+
+        results = data['Search']
+
+        # Set variable to be appended to
+        embeds = []
+
+        # Loop and ask for more information and build embed
+        for game in results:
+
+            # Queries api for a movie information
+            url = "http://www.omdbapi.com/?"+urlencode({
+                "apikey": apikey,
+                "i": game['imdbID'],
+                "plot": "full"
+            })
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url=url, headers=headers) as response:
+                    data = await response.json()
+                    log.debug(data)
+
+            # Build Embed
+            embed = discord.Embed()
+            embed.title = "{} ({})".format(data['Title'], data['Year'])
+            if data['imdbID']:
+               embed.url = "http://www.imdb.com/title/{}".format(data['imdbID'])
+            if data['Plot']:
+               embed.description = data['Plot'][:500]
+            if data['Poster'] != "N/A":
+               embed.set_thumbnail(url=data['Poster'])
+            if data['Runtime']:
+               embed.add_field(name="Runtime", value=data.get('Runtime', 'N/A'))
+            if data['Genre']:
+               embed.add_field(name="Genre", value=data.get('Genre', 'N/A'))
+            if data.get("BoxOffice"):
+               embed.add_field(name="Box Office", value=data.get('BoxOffice', 'N/A'))
+            if data['Metascore']:
+               embed.add_field(name="Metascore", value=data.get('Metascore', 'N/A'))
+            embed.set_footer(text="Powered by omdb")
+            embeds.append(embed)
+
+        await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=20)
+
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True, add_reactions=True)
+    async def tv(self, ctx, *, title):
+        """Search for a tv show"""
+
+        # Get API key
+        apikey = await self.config.apikey()
+
+        if apikey == "":
+            await ctx.send("No omdbkey set, please set one using [p]omdbkey")
+            return
+
+        url = "http://www.omdbapi.com/?"+urlencode({
+            "apikey": apikey,
+            "s": title,
+            "plot": "short",
+            "type": "series"
         })
         headers = {"accept": "application/json"}
 
